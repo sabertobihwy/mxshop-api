e05d139d5648e813adf6e523ea6616ceddc50985
wenyan <hewenyan2023gmail.com>
2023-11-08 23:08:04 +0800

    test

---
+++ user-web/utils/addr.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func GetFreePort() (int, error) {
+	addr, err := net.ResolveTCPAddr("tcp", "localhost:0")
+	if err != nil {
+		return 0, err
+	}
+	fmt.Printf("%v\n", *addr)
+	l, err := net.ListenTCP("tcp", addr)
+	if err != nil {
+		return 0, err
+	}
+	fmt.Printf("%+v\n", *l)
+	defer l.Close()
+	return l.Addr().(*net.TCPAddr).Port, nil
+}
+
+func main() {
+	l, _ := GetFreePort()
+	fmt.Println(l)
+}
